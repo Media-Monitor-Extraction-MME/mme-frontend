ddcc511ccc9766200d7c6328ef6fd88154226550
stijnveeke <47558683+stijnveeke@users.noreply.github.com>
2024-06-25 22:35:45 +0200

    Updated the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,12 @@ export const metadata = {
     'A dashboard displaying the sentiment analysis of a few user selected keywords.'
 };
 
-export default function RootLayout({
-  children
-}: Readonly<{
+type LayoutProps = {
   children: React.ReactNode;
   auth: React.ReactNode;
-}>) {
+};
+
+export default function RootLayout({ children }: Readonly<LayoutProps>) {
   return (
     <html lang="en">
       <body className="h-full" style={{ margin: 0 }}>
